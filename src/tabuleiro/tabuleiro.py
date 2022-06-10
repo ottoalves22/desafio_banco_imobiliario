273d60027cdd47049d625b774bbe81d1f621b491
otto <ottoalves@itera.com.br>
2022-06-10 10:54:39 -0300

    tabuleiro

--- src/tabuleiro/tabuleiro.py
+++ src/tabuleiro/tabuleiro.py
@@ -1,2 +1,14 @@
+from random import randint
+from propriedade import Propriedade
+
+
 class Tabuleiro:
-    pass
+    def __init__(self) -> None:
+        self.jogadores = []
+        self.rodadas = 0
+        self.vencedor = None
+        self.propriedades = [Propriedade(i) for i in range(int(20))]
+
+    def dado(self):
+        # dado de 6 lados
+        return randint(1, 6)
